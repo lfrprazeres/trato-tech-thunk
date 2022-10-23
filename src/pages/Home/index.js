a42ea3a016d97a196db4b637f899c9ecef4eae4d
Luiz Fernando <lfrprazeres@gmail.com>
2022-10-23 20:30:52 -0300

    2.2 - buscando categorias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,27 @@ import Header from 'components/Header';
 import styles from './Home.module.scss';
 import relogio from 'assets/inicial.png';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/Button';
+import { useCallback, useEffect } from 'react';
+import instance from 'common/config/api';
+import { adicionarCategorias } from 'store/reducers/categorias';
 
 export default function Home() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const categorias = useSelector(state => state.categorias);
+
+  const buscarCategorias = useCallback(async () => {
+    const resposta = await instance.get('/categorias');
+
+    dispatch(adicionarCategorias(resposta.data));
+  }, [dispatch]);
+
+  useEffect(() => {
+    buscarCategorias();
+  }, [buscarCategorias]);
+
   return (
     <div>
       <Header
