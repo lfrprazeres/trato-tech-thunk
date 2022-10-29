83c4788994027279047bcc32f838c4303d185537
Luiz Fernando <lfrprazeres@gmail.com>
2022-10-29 19:30:52 -0300

    3.2 - createAsyncThunk

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,19 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/Button';
 import { useCallback, useEffect } from 'react';
 import instance from 'common/config/api';
-import { adicionarCategorias } from 'store/reducers/categorias';
+import { adicionarCategorias, buscarCategorias } from 'store/reducers/categorias';
 import { adicionarItens } from 'store/reducers/itens';
 
 export default function Home() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const categorias = useSelector(state => state.categorias);
-
-  const buscarCategorias = useCallback(async () => {
-    const resposta = await instance.get('/categorias');
-
-    dispatch(adicionarCategorias(resposta.data));
-  }, [dispatch]);
 
   const buscarItens = useCallback(async () => {
     const resposta = await instance.get('/itens');
@@ -27,9 +21,9 @@ export default function Home() {
   }, [dispatch]);
 
   useEffect(() => {
-    buscarCategorias();
+    dispatch(buscarCategorias());
     buscarItens();
-  }, [buscarCategorias, buscarItens]);
+  }, [dispatch, buscarItens]);
 
   return (
     <div>
--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,6 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import categoriasService from 'services/categorias';
 
 const initialState = [];
+
+export const buscarCategorias = createAsyncThunk(
+  'categorias/buscar',
+  categoriasService.buscar
+);
 
 const categoriasSlice = createSlice({
   name: 'categorias',
@@ -9,6 +15,14 @@ const categoriasSlice = createSlice({
     adicionarCategorias: (state, { payload }) => {
       state.push(...payload);
     }
+  },
+  extraReducers: builder => {
+    builder.addCase(
+      buscarCategorias.fulfilled,
+      (state, { payload }) => {
+        state.push(...payload);
+      }
+    )
   }
 });
 
